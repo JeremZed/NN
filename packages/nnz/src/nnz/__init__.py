a5f3a4f7ad5d44b2a1a9fd7dbd217ad4a10dd79f
Jerem <j.thulliez@gmail.com>
2024-04-28 21:52:23 +0200

    wip class Workspace

--- packages/nnz/src/nnz/__init__.py
+++ packages/nnz/src/nnz/__init__.py
@@ -1,4 +1,27 @@
-import os,sys,platform
+import nnz.tools as tools
+from nnz.workspace import Workspace
+
+workspace = None
 
 def init():
-    print("Hello World NNZ Module", platform.uname())
+    global workspace
+    try:
+        workspace = Workspace()
+        workspace.show_informations()
+    except Exception as error:
+        tools.show_error(error)
+
+def end():
+    global workspace
+
+    try:
+        delay = tools.get_current_date() - workspace.get_date_init()
+        d = tools.get_delay(delay)
+
+        print(f'\n{tools.get_fill_string()}')
+        print("Terminé.")
+        print(f"Durée exécution : {d}")
+        print(f'{tools.get_fill_string()}\n')
+
+    except Exception as error:
+        tools.show_error(error)
---
+++ packages/nnz/src/nnz/workspace.py
@@ -0,0 +1,33 @@
+import platform
+import pkg_resources
+import nnz.tools as tools
+
+class Workspace():
+    """
+    Représente le projet en cours et son environnement
+    """
+    def __init__(self):
+
+        self.__dir_runtime = tools.create_directory("./runtime")
+        self.__date_init = tools.get_current_date()
+        self.__platform = platform.uname()
+
+    def get_date_init(self):
+        """ Permet de retourner la date où le workspace a été initialisé """
+        return self.__date_init
+
+    def show_informations(self):
+        """ Permet d'afficher l'ensemble des informations du worskpace """
+
+        print(f'\n{tools.get_fill_string()}')
+        print(f"- Date : {self.__date_init}")
+        print(f"- Répertoire runtime : {self.__dir_runtime}")
+        print(f"- Machine : {self.__platform}")
+        print(f'{tools.get_fill_string()}\n')
+        print(f'\n{tools.get_fill_string()}')
+        print(f"Liste des modules python installés :")
+        print(f'{tools.get_fill_string()}\n')
+
+        installed_packages = pkg_resources.working_set
+        for package in installed_packages:
+            print(f"{package.key}=={package.version}")
